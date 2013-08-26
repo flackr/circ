6e1bd98634b87009c4dc5a8df53001079bf9bd67
Todd Vierling <tv@duh.org>
2013-08-26 09:50:23 -0400

    Add test for sort order in the nick list.

--- test/nick_list_test.js
+++ test/nick_list_test.js
@@ -12,6 +12,9 @@
     var items = function() {
       return $('#nicks-container .nicks .nick');
     };
+    var textOfItem = function(index) {
+      return $('.content-item', item(index)).text();
+    };
     beforeEach(function() {
       mocks.dom.setUp();
       win = new chat.Window('name');
@@ -21,6 +24,20 @@
     afterEach(function() {
       win.detach();
       return mocks.dom.tearDown();
+    });
+    it("sorts entries properly", function() {
+      win.nicks.add('_psi');
+      win.nicks.add('epsilon');
+      win.nicks.add('Delta');
+      win.nicks.add('gamma');
+      win.nicks.add('Beta');
+      win.nicks.add('alpha');
+      expect(textOfItem(0)).toBe('_psi');
+      expect(textOfItem(1)).toBe('alpha');
+      expect(textOfItem(2)).toBe('Beta');
+      expect(textOfItem(3)).toBe('Delta');
+      expect(textOfItem(4)).toBe('epsilon');
+      expect(textOfItem(5)).toBe('gamma');
     });
     return it("emits a dblclicked event when a nick is double-clicked", function() {
       win.nicks.add('foo');
