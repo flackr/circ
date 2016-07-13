aea988adc03323829561268660bca7dafff9620c
Michael Forsyth <mike.forsyth23@gmail.com>
2016-07-13 15:03:53 -0400

    Add PART and NICK parsing methods for CircState and tests (#396)

--- client/js/circ-state.js
+++ client/js/circ-state.js
@@ -2,6 +2,13 @@ window.exports = window.exports || {};
 
 exports.CircState = function() {
 
+  function getUser(userPart) {
+    var source = userPart.split('!');
+    if (source.length == 1)
+      return '';
+    return source[0].substring(1);
+  }
+
   function CircState(state = {}) {
     state['channels'] = state['channels'] || {}
     this.state = state;
@@ -10,21 +17,33 @@ exports.CircState = function() {
   CircState.prototype = {
     // Override these functions to be notified about various events.
     onjoin: function(channel) {},
+    onpart: function(channel) {},
+    onnick: function(oldNick, newNick) {},
+    onownnick: function(nick) {},
 
     process: function(message) {
       var words = message.split(' ', 3);
+      var user = getUser(words[0]);
       // TODO(flackr): Check user who has joined.
-      if (words[1] == "JOIN")
-        this.state['channels'][words[2].substring(1)] = {};
-      // TODO(flackr): Check for part.
+      if (words[1] == "JOIN" && user == this.state.nick) {
+        var channel = words[2].substring(1);
+        this.state.channels[channel] = {};
+        this.onjoin(channel);
+      } else if (words[1] == "PART") {
+        var channel = words[2].substring(1);
+        if (user == this.state.nick) {
+          delete this.state.channels[channel];
+          this.onpart(channel);
+        }
+      } else if (words[1] == "NICK") {
+        var newNick = words[2].substring(1)
+        if (user == this.state.nick) {
+          this.state.nick = newNick;
+          this.onownnick(newNick);
+        }
+        this.onnick(user, newNick);
+      }
     },
-
-    getUser_: function(message) {
-      var source = message.split(' ', 1)[0].split('!');
-      if (source.length == 1)
-        return '';
-      return source[0].substring(1);
-    }
   }
 
   return CircState;
